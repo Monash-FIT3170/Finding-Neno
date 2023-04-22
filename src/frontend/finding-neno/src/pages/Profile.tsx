46e05997039d7c4c619e134ac23b27b0b1057269
Rutvik Dave <rdav0013@student.monash.edu>
2023-04-23 02:07:46 +1000

    updated app to have the 5 tabs: dashboard, map, report, sightings, profile

--- src/frontend/finding-neno/src/pages/Profile.tsx
+++ src/frontend/finding-neno/src/pages/Profile.tsx
@@ -1,25 +1,25 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
-import './Tab3.css';
+import './Tab1.css';
 
-const Tab3: React.FC = () => {
+const Profile: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>Profile</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
+            <IonTitle size="large">Profile</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+        <ExploreContainer name="Profile page" />
       </IonContent>
     </IonPage>
   );
 };
 
-export default Tab3;
+export default Profile;
