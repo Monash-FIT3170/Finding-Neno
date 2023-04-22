08f3c9743964cb485f6e053282773098fe432d38
jsch0049 <jsch0049@student.monash.edu>
2023-04-22 16:47:58 +1000

    Created back-end functions to add pets to database and to get pets from database * Added function to verify access token * Added __pycache__ to .gitignore

---
+++ src/backend/db/authentication.py
@@ -0,0 +1,29 @@
+import psycopg2
+
+def verify_access_token(connection: psycopg2.extensions.connection, user_id: int, access_token: str):
+    """
+    Verify that the user id matches the access token
+
+    Arguments:
+        connection: postgres connection
+        user_id: id of the user
+        access_token: access token of the user
+    Returns:
+        True if the user id matches the access token, False otherwise
+    """
+    cur = connection.cursor()
+
+    query = """SELECT access_token FROM users WHERE id = %s;"""
+
+    try:
+        # Execute query
+        cur.execute(query, (user_id,))
+        access_token_db = cur.fetchone()[0]
+        print(f"Query executed successfully: {query}")
+    except Exception as e:
+        print(f"Error while executing query: {e}")
+        return False
+
+    cur.close()
+
+    return access_token == access_token_db
---
+++ src/backend/db/pets.py
@@ -0,0 +1,95 @@
+import psycopg2
+from authentication import verify_access_token
+
+def get_all_pets(
+    connection: psycopg2.extensions.connection, 
+    owner_id: int,
+    access_token: str,
+):
+    """
+    Returns all pets in the database
+
+    Arguments:
+        connection: postgres connection
+        owner_id: id of the pet owner
+        access_token: access token of the pet owner
+    Returns:
+        List of pets
+    """
+    # Verify access token
+    if not verify_access_token(connection, owner_id, access_token):
+        return False
+    
+    cur = connection.cursor()
+
+    query = """SELECT * FROM pets WHERE owner_id = %s;"""
+
+    try:
+        # Execute query
+        cur.execute(query, (owner_id,))
+        pets = cur.fetchall()
+        print(f"Query executed successfully: {query}")
+    except Exception as e:
+        print(f"Error while executing query: {e}")
+        return False
+
+    cur.close()
+
+    pets = [{
+        "id": pet[0], 
+        "name": pet[1], 
+        "animal": pet[2], 
+        "breed": pet[3], 
+        "description": pet[4], 
+        "image_url": pet[5], 
+        "owner_id": pet[6]
+    } for pet in pets]
+
+    return pets
+
+
+def add_pet(
+    connection: psycopg2.extensions.connection, 
+    name: str, 
+    animal: str, 
+    breed: str, 
+    description: str, 
+    image_url: str, 
+    owner_id: int, 
+    access_token: str,
+):
+    """
+    Adds a pet to the database
+
+    Arguments:
+        connection: postgres connection
+        name: name of the pet
+        animal: type of animal
+        breed: breed of the pet
+        description: description of the pet
+        image_url: url of the image of the pet
+        owner_id: id of the pet owner
+        access_token: access token of the pet owner
+    Returns:
+        True if successful, False otherwise
+    """
+    # Verify access token
+    if not verify_access_token(connection, owner_id, access_token):
+        return False
+
+    cur = connection.cursor()
+
+    query = """INSERT INTO pets (name, animal, breed, description, image_url, owner_id) VALUES (%s, %s, %s, %s, %s, %s);"""
+
+    try:
+        # Execute query
+        cur.execute(query, (name, animal, breed, description, image_url, owner_id,))
+        print(f"Query executed successfully: {query}")
+    except Exception as e:
+        print(f"Error while executing query: {e}")
+        return False
+
+    cur.close()
+    connection.commit()
+
+    return True
