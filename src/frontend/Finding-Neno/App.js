f0d23cfa4448ffd33f385a325bbe88f82fe95b2e
Adam Ye <ayee0007@student.monash.edu>
2023-04-25 20:58:19 +1000

    Added signup page, form validation and routing between signup and login pages.

--- src/frontend/Finding-Neno/App.js
+++ src/frontend/Finding-Neno/App.js
@@ -1,18 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text } from 'react-native';
-import { 
-  NativeBaseProvider, 
-  Box } from "native-base";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text } from "react-native";
+import { useState } from "react";
+import { NativeBaseProvider, Box } from "native-base";
 
-import LoginPage from './components/Login/LoginPage'
+import LoginPage from "./components/Login/LoginPage";
+import SignupPage from "./components/Login/SignupPage";
 
 export default function App() {
+  const [isLogin, setIsLogin] = useState(true); // true to show login page, false to show signup page
+  const switchPage = () => {
+    setIsLogin(!isLogin);
+  };
+
+  const login = (formData) => {
+    alert("login data: " + JSON.stringify(formData));
+  };
+
+  const signup = (formData) => {
+    alert("signup data: " + JSON.stringify(formData));
+  };
+
   return (
     <NativeBaseProvider>
       {/* remove this css later */}
       <Box flex={1} alignItems="center" justifyContent="center">
         {/* keeping this here for now */}
-        <LoginPage onPress={() => alert('testing onpress')}/>
+
+        {isLogin ? (
+          <LoginPage onLoginPress={login} onSwitchPress={switchPage} />
+        ) : (
+          <SignupPage onSignupPress={signup} onSwitchPress={switchPage} />
+        )}
       </Box>
     </NativeBaseProvider>
   );
@@ -21,8 +39,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
