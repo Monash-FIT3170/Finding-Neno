0f7b483bbfe7ff57be006c365831bc52a673ae4b
Adam Ye <ayee0007@student.monash.edu>
2023-04-30 22:25:11 +1000

    added dotenv for frontend to store IP in .env file

--- src/backend/api/user_operations.py
+++ src/backend/api/user_operations.py
@@ -13,34 +13,35 @@ from db.users_operations import insert_user
 app = Flask(__name__)
 conn = None
 
-@app.route('/')
+
+@app.route("/")
 def root():
-    return 'Finding Neno Server is Up!'
+    return "Finding Neno Server is Up!"
 
-@app.route('/insert_user', methods=['POST'])
+
+@app.route("/insert_user", methods=["POST"])
 def post_insert_user():
-    json_data = request.get_json(force=True) 
-    print('inserting user: ', json_data)
-    email = json_data['email']
-    phoneNumber = json_data['phoneNumber']
-    password = json_data['password']
-    username = 'John' # TODO signup with username?
+    json_data = request.get_json(force=True)
+    print("inserting user: ", json_data)
+    email = json_data["email"]
+    phoneNumber = json_data["phoneNumber"]
+    password = json_data["password"]
+    username = "John"  # TODO signup with username?
     insert_user(conn, email, phoneNumber, username, password)
-    return '', 201  
-    
+    return "", 201
 
 
-
-
-
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     # Get environment file path from command line arguments
     if len(sys.argv) < 2:
-        raise Exception("No environment file path provided - see top of this file for instructions")
+        raise Exception(
+            "No environment file path provided - see top of this file for instructions"
+        )
     environment_file_path = sys.argv[1]
     if environment_file_path is None or environment_file_path == "":
-        raise Exception("No environment file path provided - see top of this file for instructions")
+        raise Exception(
+            "No environment file path provided - see top of this file for instructions"
+        )
     else:
         # Load environment variables
         load_dotenv(environment_file_path)
@@ -54,4 +55,4 @@ if __name__ == '__main__':
             port=os.getenv("DATABASE_PORT"),
         )
 
-        app.run(host='0.0.0.0', debug=True)
+        app.run(host="0.0.0.0", debug=True)
