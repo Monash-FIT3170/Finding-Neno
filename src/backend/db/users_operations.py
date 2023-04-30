450772a73b4ef83fbae16163eff0052f019804c2
Zahin Shahriar Bin Khair <zahin.shahriar.82@gmail.com>
2023-04-30 14:50:43 +1000

    Added documentation

--- src/backend/db/users_operations.py
+++ src/backend/db/users_operations.py
@@ -15,7 +15,8 @@ def salt_and_hash(password): #TODO: IMPLEMENT
 
     return ""
 
-def generate_access_token(): #TODO: IMPLEMENT
+def generate_access_token():
+    # This function generates an 32 character random access token using both letters and numbers
 
     alphabet = string.ascii_letters + string.digits
     token = ''.join(secrets.choice(alphabet) for i in range(32))
@@ -23,22 +24,26 @@ def generate_access_token(): #TODO: IMPLEMENT
     return token
 
 def insert_user(email, phone, user_name, password):
+    # This function is used to add a new user to the database
 
     cur = conn.cursor()
 
+    # Hash the password input
     hashed_pass = salt_and_hash(password)
 
+    # Generate an access token
     access_token = generate_access_token()
 
+    # Construct and INSERT query to insert this user into the DB
     query = """INSERT INTO users (email_address, phone_number, name, password, access_token) VALUES (%s, %s, %s, %s, %s);"""
 
     # Execute the query
-
     try:
         cur.execute(query, (email, phone, user_name, hashed_pass, access_token))
         print(f"Query executed successfully: {query}")
     except Exception as e:
         print(f"Error while executing query: {e}")
 
+    # Commit the change and close the connection
     conn.commit()
     cur.close()
