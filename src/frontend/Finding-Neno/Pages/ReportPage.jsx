6d9d1f10872a6bd2f1ee4819c4a3a7cacc353147
jsch0049 <jsch0049@student.monash.edu>
2023-05-01 19:45:14 +1000

    Implemented page skeletons, router and tab navigation for front-end
    Co-authored-by: Rutvik Dave <rdave24@users.noreply.github.com>

---
+++ src/frontend/Finding-Neno/Pages/ReportPage.jsx
@@ -0,0 +1,8 @@
+import { NavigationContainer, useNavigation  } from '@react-navigation/native';
+import { Text } from 'react-native';
+
+export default function ReportPage() {
+    const navigation = useNavigation();
+
+    return (<Text>Report page</Text>)
+}
