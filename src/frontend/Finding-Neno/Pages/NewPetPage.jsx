c50d7221aecf08fa73977b67bc1645f1dd3b55a9
gfra12 <gfra12@student.monash.edu>
2023-05-02 10:11:25 +1000

    Form components complete.

--- src/frontend/Finding-Neno/Pages/NewPetPage.jsx
+++ src/frontend/Finding-Neno/Pages/NewPetPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, Image } from 'react-native';
+import { View, Text, TextInput, Button, Image, ScrollView } from 'react-native';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import * as ImagePicker from 'expo-image-picker';
 import { Picker } from '@react-native-picker/picker';
@@ -9,8 +9,8 @@ export default function NewPetPage() {
 
     const [petName, setPetName] = useState('');
     const [petImage, setPetImage] = useState(null);
+    const [petType, setPetType] = useState('');
     const [petBreed, setPetBreed] = useState('');
-    const [petAge, setPetAge] = useState('');
     const [petGender, setPetGender] = useState('');
 
     const handleChoosePhoto = async () => {
@@ -22,18 +22,20 @@ export default function NewPetPage() {
             aspect: [4, 3],
             quality: 1,
           });
-          if (!result.cancelled) {
+          if (!result.canceled) {
             setPetImage(result.uri);
           }
         }
       };
 
     const handleSubmit = () => {
-        // code to submit the pet details
+        alert(petName + ' ' + petType + ' ' + petBreed + ' ' + petGender);
     };
 
 
     return (
+
+        <ScrollView>
         
         <View style={{ padding: 16 }}>
 
@@ -54,10 +56,21 @@ export default function NewPetPage() {
             />
         </View>
 
-        <Text>Pet Image</Text>
+        <Text style={{ marginBottom: 8, fontSize: 16 }}>Pet Image</Text>
         {petImage && <Image source={{ uri: petImage }} style={{ width: 200, height: 200 }} />}
         <Button title="Choose Photo" onPress={handleChoosePhoto} />
     
+        <Text style={{  fontSize: 16 }}>Pet Type</Text>
+        <Picker selectedValue={petType} onValueChange={setPetType} >
+            <Picker.Item label="Dog" value="dog" />
+            <Picker.Item label="Cat" value="cat" />
+            <Picker.Item label="Rabbit" value="rabbit" />
+            <Picker.Item label="Mouse" value="mouse" />
+            <Picker.Item label="Snake" value="snake" />
+            <Picker.Item label="Bird" value="bird" />
+            <Picker.Item label="Other" value="other" />
+        </Picker>
+
         <Text style={{ marginBottom: 8, fontSize: 16 }}>Pet breed:</Text>
         <View
             style={{
@@ -74,11 +87,22 @@ export default function NewPetPage() {
                 onChangeText={setPetBreed}
             />
         </View>
+
+
+        <Text style={{fontSize: 16 }}>Pet Gender</Text>
+        
+            <Picker selectedValue={petGender} onValueChange={setPetGender}>
+                <Picker.Item label="Male" value="Male" />
+                <Picker.Item label="Female" value="Female" />
+            </Picker>
+        
           
     
           <Button title="Submit" onPress={handleSubmit} />
         </View>
+
+        </ScrollView>
       );
     
-    //return (<Text>New pet page</Text>)
+
 }
