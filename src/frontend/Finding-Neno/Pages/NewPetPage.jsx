746fa31819c1e5296497aa605d921403b3ae188f
gfra12 <gfra12@student.monash.edu>
2023-05-02 15:20:27 +1000

    completed and tested add pet to database with form

--- src/frontend/Finding-Neno/Pages/NewPetPage.jsx
+++ src/frontend/Finding-Neno/Pages/NewPetPage.jsx
@@ -3,15 +3,17 @@ import { View, Text, TextInput, Button, Image, ScrollView } from 'react-native';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import * as ImagePicker from 'expo-image-picker';
 import { Picker } from '@react-native-picker/picker';
+import { KeyboardAvoidingView } from 'react-native';
+import { IP, PORT } from "@env";
 
-export default function NewPetPage() {
+export default function NewPetPage( {this_user} ) {
     const navigation = useNavigation();
 
     const [petName, setPetName] = useState('');
     const [petImage, setPetImage] = useState(null);
     const [petType, setPetType] = useState('');
     const [petBreed, setPetBreed] = useState('');
-    const [petGender, setPetGender] = useState('');
+    const [petDescription, setPetDescription] = useState('');
 
     const handleChoosePhoto = async () => {
         const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
@@ -23,21 +25,50 @@ export default function NewPetPage() {
             quality: 1,
           });
           if (!result.canceled) {
-            setPetImage(result.uri);
+            setPetImage(result.assets[0].uri);
           }
         }
       };
 
     const handleSubmit = () => {
-        alert(petName + ' ' + petType + ' ' + petBreed + ' ' + petGender);
+
+        const url = `${IP.toString()}:${PORT.toString()}/insert_pet`;
+
+        const pet = {
+            name: petName,
+            animal: petType,
+            breed: petBreed,
+            description: petDescription,
+            image_url: petImage.toString(),
+            owner_id: this_user.id       
+        };
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${this_user.token}`,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(pet),
+        })
+        .then((res) => {
+            if (res.status == 201) {
+              alert("Inserted pet successfully");
+            }
+            else {
+                alert("Error");
+            }
+          })
+          .catch((error) => alert(error));
     };
 
-
     return (
-
-        <ScrollView>
         
+        <ScrollView>
+        <KeyboardAvoidingView behavior="padding" enabled keyboardVerticalOffset={250}>
         <View style={{ padding: 16 }}>
+        
+        
+        
 
         <Text style={{ marginBottom: 8, fontSize: 16 }}>Pet name:</Text>
         <View
@@ -89,19 +120,33 @@ export default function NewPetPage() {
         </View>
 
 
-        <Text style={{fontSize: 16 }}>Pet Gender</Text>
-        
-            <Picker selectedValue={petGender} onValueChange={setPetGender}>
-                <Picker.Item label="Male" value="Male" />
-                <Picker.Item label="Female" value="Female" />
-            </Picker>
-        
-          
-    
-          <Button title="Submit" onPress={handleSubmit} />
+        <Text style={{ marginBottom: 8, fontSize: 16 }}>Pet Description:</Text>
+        <View
+            style={{
+                borderWidth: 1,
+                borderRadius: 8,
+                borderColor: '#ddd',
+                padding: 8,
+                marginBottom: 16,
+            }}>
+            <TextInput
+                multiline
+                style={{ fontSize: 16 }}
+                placeholder="Enter pet description"
+                value={petDescription}
+                onChangeText={setPetDescription}
+            />
         </View>
+    
+        <Button title="Submit" onPress={handleSubmit} />
 
+
+        
+        </View>
+        
+        </KeyboardAvoidingView>
         </ScrollView>
+        
       );
     
 
