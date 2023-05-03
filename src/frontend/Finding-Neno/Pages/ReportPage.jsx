f5d0c8edd0dde819979a1636da93e72ad75c656e
rdev0010 <rdev0010@student.monash.edu>
2023-05-03 15:30:34 +1000

    started setting up report page

--- src/frontend/Finding-Neno/Pages/ReportPage.jsx
+++ src/frontend/Finding-Neno/Pages/ReportPage.jsx
@@ -1,8 +1,14 @@
 import { NavigationContainer, useNavigation  } from '@react-navigation/native';
-import { Text } from 'react-native';
+import { View, Text, TextInput, Button, ScrollView } from 'react-native';
+
+import { Color } from "../components/atomic/Theme";
+import { useState } from "react";
+import { IP, PORT } from "@env";
 
 export default function ReportPage() {
     const navigation = useNavigation();
 
-    return (<Text>Report page</Text>)
-}
+    const [missingPet, setMissingPet] = useState('')
+    const [lastSeen, setLastSeen] = useState('')
+    const [lastLocation, setLastLocation] = useState('')
+};
