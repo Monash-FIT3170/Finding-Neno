23b9e5516115cfb341a96e80ab3f59d9277cf74c
Vincent Wijaya <vwij0004@student.monash.edu>
2023-05-06 00:29:20 +1000

    Changes to backend - Made main.py which creates database pool and imports functions from user_operations to be used. - Moved app initialisation and DB pool from user_operations to main

---
+++ src/backend/api/main.py
@@ -0,0 +1,59 @@
+import psycopg2
+import psycopg2.pool
+import sys, os
+from dotenv import load_dotenv
+from flask import Flask
+
+from user_operations import insert_user
+
+
+database_pool = None
+
+app = Flask(__name__)
+
+# Creates a PostgreSQL connection pool which can be accessed by multiple instances.
+def create_database_pool():
+    return psycopg2.pool.SimpleConnectionPool(
+        minconn=1,
+        maxconn=10,
+        dbname=os.getenv("DATABASE_NAME"),
+        user=os.getenv("DATABASE_USER"),
+        password=os.getenv("DATABASE_PASSWORD"),
+        host=os.getenv("DATABASE_HOST"),
+        port=os.getenv("DATABASE_PORT")
+    )
+
+
+def get_db():
+    if database_pool is not None:
+        return database_pool
+    
+
+@app.route("/")
+def root():
+    return "Finding Neno Server is Up!"
+
+
+@app.route("/insert_user", methods=["POST"])
+def post_insert_user():
+    insert_user()
+
+if __name__ == "__main__":
+    # Get environment file path from command line arguments
+    if len(sys.argv) < 2:
+        raise Exception(
+            "No environment file path provided - see top of this file for instructions"
+        )
+    environment_file_path = sys.argv[1]
+    if environment_file_path is None or environment_file_path == "":
+        raise Exception(
+            "No environment file path provided - see top of this file for instructions"
+        )
+    else:
+        # Load environment variables
+        load_dotenv(environment_file_path)
+
+        database_pool = create_database_pool()
+
+        app.run(host="0.0.0.0", debug=True)
+
--- src/backend/api/user_operations.py
+++ src/backend/api/user_operations.py
@@ -1,9 +1,6 @@
-from flask import Flask, request, jsonify
-import psycopg2
-import psycopg2.pool
-import sys, os
-from dotenv import load_dotenv
+from flask import request, jsonify
 from pathlib import Path
+import sys
 
 file = Path(__file__).resolve()
 package_root_directory = file.parents[1]
@@ -11,29 +8,7 @@ sys.path.append(str(package_root_directory))
 
 from db.users_operations import insert_user
 
-app = Flask(__name__)
-conn = None
-database_pool = None
-
-def create_database_pool():
-    return psycopg2.pool.SimpleConnectionPool(
-        minconn=1,
-        maxconn=10,
-        dbname=os.getenv("DATABASE_NAME"),
-        user=os.getenv("DATABASE_USER"),
-        password=os.getenv("DATABASE_PASSWORD"),
-        host=os.getenv("DATABASE_HOST"),
-        port=os.getenv("DATABASE_PORT")
-    )
-
-
-@app.route("/")
-def root():
-    return "Finding Neno Server is Up!"
-
-
-@app.route("/insert_user", methods=["POST"])
-def post_insert_user():
+def insert_user():
     json_data = request.get_json(force=True)
     print("inserting user: ", json_data)
     email = json_data["email"]
@@ -42,27 +17,3 @@ def post_insert_user():
     username = "John"  # TODO signup with username?
     insert_user(conn, email, phoneNumber, username, password)
     return "", 201
-
-if __name__ == "__main__":
-    # Get environment file path from command line arguments
-    if len(sys.argv) < 2:
-        raise Exception(
-            "No environment file path provided - see top of this file for instructions"
-        )
-    environment_file_path = sys.argv[1]
-    if environment_file_path is None or environment_file_path == "":
-        raise Exception(
-            "No environment file path provided - see top of this file for instructions"
-        )
-    else:
-        # Load environment variables
-        load_dotenv(environment_file_path)
-
-        database_pool = create_database_pool()
-
-        app.run(host="0.0.0.0", debug=True)
-
-
-def get_db():
-    if database_pool is not None:
-        return database_pool
