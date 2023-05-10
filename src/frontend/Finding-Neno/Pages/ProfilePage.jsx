fbca1025e4e85a14450bdc3c951defbdcea8e6dd
Rutvik Dave <rdav0013@student.monash.edu>
2023-05-10 13:17:43 +1000

    fixed layout, added dummy boxes which will be replaced with pet boxes

--- src/frontend/Finding-Neno/Pages/ProfilePage.jsx
+++ src/frontend/Finding-Neno/Pages/ProfilePage.jsx
@@ -1,6 +1,5 @@
 import { NavigationContainer, useNavigation  } from '@react-navigation/native';
-import { Button } from 'react-native';
-import { Box, Center, Image, Heading, HStack, VStack, Link, Text} from "native-base";
+import { Box, Image, Heading, HStack, VStack, Button, Text, ScrollView, Center} from "native-base";
 import {Dimensions} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import { Color } from "../components/atomic/Theme";
@@ -23,120 +22,134 @@ const App = () => {
         This is a Box with Linear Gradient
       </Box>;
   };
-  
-  const config = {
-    dependencies: {
-      'linear-gradient': LinearGradient
-    }
-  };
-  
 
-export default function ProfilePage({ navigation: { navigate}}) {
+
+export default function ProfilePage({ navigation: { navigate}}, this_user) {
     const navigation = useNavigation();
     const windowWidth = Dimensions.get('window').width; 
     const windowHeight = Dimensions.get('window').height;
+
+    {/. Call a get user function ./}
+
     const name = "Human Being";
-    const email = "sample@student.monash.edu"
-    const phone = "0412 345 678"
+    const email = "sample@student.monash.edu";
+    const phone = "0412 345 678";
 
     return (
-        <Box alignItems="center" justifyContent="center">
-            <Center>        
-            <Box>
-              <Box 
-                alignSelf="center"
-                _text={{
-                  alignSelf:"center",
-                  justifyContent:"center",
-                  fontSize: "lg",
-                  fontWeight: "medium",
-                  color: "warmGray.50",
-                  letterSpacing: "lg"
-                }} 
-                bg={Color.NENO_BLUE}
-                width={windowWidth}
-                height={windowHeight/8}
-              >
-                <Box height={3}/>
-                <HStack>
-                <Box width={8}/>
-                <Box
-                  bg="#FFFFFF"
-                  height={76}
-                  width={76}
-                  borderRadius={38}
-                  alignSelf="center"
-                  alignItems="center"
-                  justifyContent="center"
-                >
-                  <Image 
-                    alignSelf="center" size={70} borderRadius={35} 
-                    source={{
-                    uri: "https://wallpaperaccess.com/full/317501.jpg"
-                    }} alt="Alternate Text" 
-                  />
+      <ScrollView>
+      <Box alignItems="center" justifyContent="center">        
+        <Box 
+          alignSelf="center"
+          _text={{
+            alignSelf:"center",
+            justifyContent:"center",
+            fontSize: "lg",
+            fontWeight: "medium",
+            color: "warmGray.50",
+            letterSpacing: "lg"
+          }} 
+          bg={Color.NENO_BLUE}
+          width={windowWidth}
+          height={windowHeight/8}
+        >
+        <Box height={3}/>
+        <HStack>
+          <Box width={8}/>
+          <Box
+            bg="#FFFFFF"
+            height={76}
+            width={76}
+            borderRadius={38}
+            alignSelf="center"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Image 
+              alignSelf="center" size={70} borderRadius={35} 
+              source={{
+                uri: "https://wallpaperaccess.com/full/317501.jpg"
+              }} 
+              alt="Alternate Text" 
+            />
+          </Box>
+          <Box width={9}/>
+          <Heading alignSelf="center" size="lg" fontWeight="600" color="warmGray.200" _dark={{color: "coolGray.600",}} >
+            {name}
+          </Heading>
+        </HStack>
+      </Box>
 
-                </Box>
-                <Box width={9}/>
-                <Heading
-                    alignSelf="center"
-                    size="lg"
-                    fontWeight="600"
-                    color="warmGray.50"
-                    >
-                    {name}
-                </Heading>
-                </HStack>
-              </Box>
-            </Box>
 
-        <VStack>
-          <HStack mt="6" justifyContent="space-between">
+      <VStack>
+        <HStack mt="6" justifyContent="space-between">
           <Heading fontSize="sm" color="coolGray.600" _dark={{color: "warmGray.200",}} pr={windowWidth/3.5}>
             USER DETAILS
           </Heading>
           <Text pl={windowWidth/3.5}>
           </Text>
-          </HStack>
-          
-          <HStack mt="2" justifyContent="space-between">
-          <Heading fontSize="sm" color="coolGray.600" _dark={{color: "warmGray.200",}}>
-              Email
-            </Heading>
-            <Text fontSize="sm" color="coolGray.600" _dark={{color: "warmGray.200",}}>
-              {email}
-            </Text>
-          </HStack>
-          
-          <HStack mt="2" justifyContent="space-between">
-            <Heading fontSize="sm" color="coolGray.600" _dark={{color: "warmGray.200",}}>
-              Phone
-            </Heading>
-            <Text fontSize="sm" color="coolGray.600" _dark={{color: "warmGray.200",}}>
-              {phone}
-            </Text>
-          </HStack>
-        </VStack>
-        
-        <Box width={8}/>
+        </HStack>
 
-        <VStack>
+        <Box h="2"></Box>
+          
+        <Box bg = "gray.200" px="2" py="1" borderRadius="md"> 
+        <HStack mt="2" justifyContent="space-between">
+          <Heading fontSize="sm" color="coolGray.600" _dark={{color: "warmGray.200",}}>
+            Email
+          </Heading>
+          <Text fontSize="sm" color="coolGray.600" _dark={{color: "warmGray.200",}}>
+            {email}
+          </Text>
+        </HStack>
+        </Box> 
+        
+        <Box h="2"></Box>
+
+        <Box bg = "gray.200" px="2" py="1" borderRadius="md">   
+        <HStack mt="2" justifyContent="space-between">
+          <Heading fontSize="sm" color="coolGray.600" _dark={{color: "warmGray.200",}}>
+            Phone
+          </Heading>
+          <Text fontSize="sm" color="coolGray.600" _dark={{color: "warmGray.200",}}>
+            {phone}
+          </Text>
+        </HStack>
+        </Box> 
+        
+      </VStack>
+        
+      <Box height={1}/>
+
+      <VStack>
         <HStack mt="6" justifyContent="space-between">
           <Heading fontSize="sm" color="coolGray.600" _dark={{color: "warmGray.200",}} pr={windowWidth/3.5}>
             PETS     
           </Heading>
           <Text pl={windowWidth/2.2}>
           </Text>
-          </HStack>
+        </HStack>
+        
+        <Box width={windowWidth - 20} height={100} bg={Color.NENO_BLUE}/>
+        <Box height={1}/>
+        <Box width={windowWidth - 20} height={100} bg={Color.NENO_BLUE}/>
+        <Box height={1}/>
+        <Box width={windowWidth - 20} height={100} bg={Color.NENO_BLUE}/>
+        <Box height={1}/>
+        <Box width={windowWidth - 20} height={100} bg={Color.NENO_BLUE}/>
+        <Box height={1}/>
 
-        </VStack>
-            
-            <Button
-                onPress={() => navigate('New Pet Page')} 
-                title="Add New Pet" /> 
-            
-            </Center>
-        </Box>
-                            
+      </VStack>
+      
+      <Box h="4"></Box>
+
+      <Button
+        onPress={() => navigate('New Pet Page')} 
+        width={windowWidth - 100}
+        height="40px"
+      >
+        Add New Pet
+      </Button> 
+           
+    </Box>
+    </ScrollView>                        
     )
 }
