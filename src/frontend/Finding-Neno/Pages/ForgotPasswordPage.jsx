5bf374f96693904fd9773ec95ca727dc6a4ac591
rdev0010 <rdev0010@student.monash.edu>
2023-05-10 23:13:53 +1000

    Created forgot password page

---
+++ src/frontend/Finding-Neno/Pages/ForgotPasswordPage.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+import {View, Text, TextInput, ScrollView,} from 'react-native';
+
+const ForgotPasswordPage = () => {
+};
+
+export default ForgotPasswordPage;
