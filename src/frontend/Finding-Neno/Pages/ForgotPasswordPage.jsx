a181a7fa63f78eb57ff1bdfcf87c753666f0aa53
rdev0010 <rdev0010@student.monash.edu>
2023-05-10 23:38:31 +1000

    added button to reset password

--- src/frontend/Finding-Neno/Pages/ForgotPasswordPage.jsx
+++ src/frontend/Finding-Neno/Pages/ForgotPasswordPage.jsx
@@ -1,7 +1,40 @@
-import React, { useState } from 'react';
-import {View, Text, TextInput, ScrollView,} from 'react-native';
+import { Box, Center, Heading, VStack, HStack, FormControl, Input, Link, Button, Text } from "native-base";
+import { NavigationContainer, useNavigation  } from '@react-navigation/native';
+
+import { Color } from "../components/atomic/Theme";
+import {validEmail} from "./validation"
+import { useState } from "react";
+import { IP, PORT } from "@env";
 
 const ForgotPasswordPage = () => {
+    const [formData, setFormData] = useState({});
+    const [errors, setErrors] = useState({});
+
+    const navigation = useNavigation();
+
+    return (
+        <Box flex={1} alignItems="center" justifyContent="center">
+            <Center w="100%">
+                <Box safeArea p="2" py="8" w="90%" maxW="290">
+                    <Heading
+                        size="lg"
+                        fontWeight="600"
+                        color="coolGray.800"
+                        _dark={{
+                        color: "warmGray.50",
+                        }}
+                    >
+                        Password Reset
+                    </Heading>
+
+                    <Button mt="2" bgColor={Color.NENO_BLUE} onPress={validateDetails}>
+                        Reset Password
+                    </Button>
+
+                </Box>
+            </Center>
+        </Box>
+    );
 };
 
 export default ForgotPasswordPage;
