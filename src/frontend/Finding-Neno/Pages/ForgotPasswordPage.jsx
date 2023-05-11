6f5b518ed77ab30880ba8211bedbdf3117e7c48c
rdev0010 <rdev0010@student.monash.edu>
2023-05-12 00:24:29 +1000

    added email validation

--- src/frontend/Finding-Neno/Pages/ForgotPasswordPage.jsx
+++ src/frontend/Finding-Neno/Pages/ForgotPasswordPage.jsx
@@ -13,27 +13,17 @@ const ForgotPasswordPage = () => {
     const navigation = useNavigation();
 
     const onForgotPasswordPress = (formData) => {
-        alert("password reset data: " + JSON.stringify(formData));
+        alert("forgot password data: " + JSON.stringify(formData));
       };
 
       const validateDetails = () => {
         // Validates details. If details are valid, send formData object to onForgotPasswordPress.
         foundErrors = {};
     
-        if (!formData.resetCode || formData.resetCode == "") {
-          foundErrors = {...foundErrors, resetCode: 'Reset Code is required'}
-        }
-    
-        if (!formData.password || formData.password == "") {
-          foundErrors = {...foundErrors, password: 'Password is required'}
-        }
-    
-        if (!formData.confirmPassword || formData.confirmPassword == "") {
-          foundErrors = {...foundErrors, confirmPassword: 'Password confirmation is required'}
-        }
-    
-        if (formData.confirmPassword !== formData.password) {
-          foundErrors = {...foundErrors, confirmPassword: 'Passwords must match'}
+        if (!formData.email || formData.email == "") {
+          foundErrors = {...foundErrors, email: 'Email is required'}
+        } else if (!validEmail(formData.email)) {
+          foundErrors = {...foundErrors, email: 'Email is invalid'}
         }
     
         setErrors(foundErrors);
@@ -45,49 +35,6 @@ const ForgotPasswordPage = () => {
       }
 
     return (
-        <Box flex={1} alignItems="center" justifyContent="center">
-            <Center w="100%">
-                <Box safeArea p="2" py="8" w="90%" maxW="290">
-                    
-                    <Heading
-                        size="lg"
-                        fontWeight="600"
-                        color="coolGray.800"
-                        _dark={{
-                        color: "warmGray.50",
-                        }}
-                    >
-                        Password Reset
-                    </Heading>
-
-                    <VStack space={3} mt="5">
-
-                        <FormControl isInvalid={'resetCode' in errors}>
-                            <FormControl.Label>Reset Code</FormControl.Label>
-                            <Input onChangeText={value => setFormData({...formData, resetCode: value})} />
-                            {'resetCode' in errors && <FormControl.ErrorMessage>{errors.resetCode}</FormControl.ErrorMessage>}
-                        </FormControl>
-
-                        <FormControl isInvalid={'password' in errors}>
-                            <FormControl.Label>Password</FormControl.Label>
-                            <Input type="password" onChangeText={value => setFormData({...formData, password: value})} />
-                            {'password' in errors && <FormControl.ErrorMessage>{errors.password}</FormControl.ErrorMessage>}
-                        </FormControl>
-
-                        <FormControl isInvalid={'confirmPassword' in errors}>
-                            <FormControl.Label>Confirm Password</FormControl.Label>
-                            <Input type="password" onChangeText={value => setFormData({...formData, confirmPassword: value})} />
-                            {'confirmPassword' in errors && <FormControl.ErrorMessage>{errors.confirmPassword}</FormControl.ErrorMessage>}
-                        </FormControl>
-
-                        <Button mt="2" bgColor={Color.NENO_BLUE} onPress={validateDetails}>
-                            Reset Password
-                        </Button>
-
-                    </VStack>
-                </Box>
-            </Center>
-        </Box>
     );
 };
 
