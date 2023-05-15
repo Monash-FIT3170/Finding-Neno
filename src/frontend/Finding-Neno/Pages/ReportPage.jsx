2710ae82cfc4866f030890fcc68fb9acbe0f44cf
Vincent Wijaya <vwij0004@student.monash.edu>
2023-05-16 01:39:20 +1000

    Added onCreateReportPress func

--- src/frontend/Finding-Neno/Pages/ReportPage.jsx
+++ src/frontend/Finding-Neno/Pages/ReportPage.jsx
@@ -16,6 +16,62 @@ const ReportPage = () => {
   const [dropdownHeight, setDropdownHeight] = useState(150);
 
 
+  const onCreateReportPress = async (formData) => {
+    const url = `${IP}:${PORT}/insert_missing_report`;
+ 
+    fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formData),
+    })
+      .then((res) => {
+        if (res.status == 201) {
+          setIsRegistered(true);
+        }
+      })
+      .catch((error) => alert(error));
+  };
+
+  const validateDetails = () => {
+    // Validates details. If details are valid, send formData object to onSignupPress.
+    foundErrors = {};
+
+    // if (!formData.name || formData.name == "") {
+    //   foundErrors = {...foundErrors, name: 'Name is required'}
+    // }
+
+    // if (!formData.email || formData.email == "") {
+    //   foundErrors = {...foundErrors, email: 'Email is required'}
+    // } else if (!validEmail(formData.email)) {
+    //   foundErrors = {...foundErrors, email: 'Email is invalid'}
+    // }
+
+    // if (!formData.phoneNumber || formData.phoneNumber == "") {
+    //   foundErrors = {...foundErrors, phoneNumber: 'Phone number is required'}
+    // } else if (!validPhoneNumber(formData.phoneNumber)) {
+    //   foundErrors = {...foundErrors, phoneNumber: 'Phone number is invalid'}
+    // }
+
+    // if (!formData.password || formData.password == "") {
+    //   foundErrors = {...foundErrors, password: 'Password is required'}
+    // }
+
+    // if (!formData.confirmPassword || formData.confirmPassword == "") {
+    //   foundErrors = {...foundErrors, confirmPassword: 'Password confirmation is required'}
+    // }
+
+    // if (formData.confirmPassword !== formData.password) {
+    //   foundErrors = {...foundErrors, confirmPassword: 'Passwords must match'}
+    // }
+
+    setErrors(foundErrors);
+
+    if (Object.keys(foundErrors).length === 0) {
+      // no errors!
+      onSignupPress(formData)
+    }
+  }
+
   return (
     <ScrollView>
         <View style={{ padding: 16 }}>
