57e1e906f6da3178ea474529977e99df00d38046
Vincent Wijaya <vwij0004@student.monash.edu>
2023-05-21 12:01:13 +1000

    Limited date time to present or past

--- src/frontend/Finding-Neno/Pages/NewReportPage.jsx
+++ src/frontend/Finding-Neno/Pages/NewReportPage.jsx
@@ -1,5 +1,8 @@
 import { Box, Center, Heading, VStack, FormControl, Input, Button, Select, Alert, Text, KeyboardAvoidingView } from "native-base";
-import DateTimePickerModal from 'react-native-modal-datetime-picker'
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import DatePicker from 'react-native-datepicker'
+
 
 import React, { useEffect, useState } from 'react';
 import { Color } from "../components/atomic/Theme";
@@ -49,23 +52,6 @@ const NewReportPage = () => {
     fetchOwnerPets();
   }, []);
 
-  const handleConfirm = (date) => {
-    setShowPicker(false);
-    if (date) {
-      setSelectedDate(date);
-      setFormData({...formData, lastSeenDateTime: selectedDatetime})
-    }
-  };
-
-  const handleCancel = () => {
-    setShowPicker(false);
-  }
-
-  var maximumDate;
-  const openPicker = () => {
-    maximumDate = new Date();
-    setShowPicker(true);
-  };
 
   const onCreateReportPress = async (formData) => {
     const url = `${IP}:${PORT}/insert_missing_report`;
@@ -89,20 +75,23 @@ const NewReportPage = () => {
     // Validates details. If details are valid, send formData object to onCreateReportPress.
     foundErrors = {};
 
-    if (!formData.missingPetId) {
-      foundErrors = {...foundErrors, missingPetId: 'Please select a pet'}
-    }
+    // if (!formData.missingPetId) {
+    //   foundErrors = {...foundErrors, missingPetId: 'Please select a pet'}
+    // }
 
-    if (!formData.lastSeenDateTime || formData.lastSeenDateTime) {
+    console.log(selectedDatetime >= new Date())
+    if (!formData.lastSeenDateTime) {
       foundErrors = {...foundErrors, lastSeenDateTime: 'Last seen date is required'}
-    } else if (!validDateTime(formData.lastSeenDateTime)) {
-      foundErrors = {...foundErrors, lastSeenDateTime: 'Date is invalid'}
+    // } else if (!validDateTime(formData.lastSeenDateTime)) {
+    } else if (selectedDatetime >= new Date()) {
+      foundErrors = {...foundErrors, lastSeenDateTime: 'Last seen date cannot be in the future'}
     }
-
+    // formData.lastSeenDateTime = formatDatetimeString(formData.lastSeenDateTime)
+    
     if (!formData.lastLocation || formData.lastLocation == "") {
-      foundErrors = {...foundErrors, lastLocation: 'Last known location is required'}
+      foundErrors = {...foundErrors, lastLocation: 'Last known location is required e.g. 24.212, -54.122'}
     } else if (!validateCoordinates(formData.lastLocation)) {
-      foundErrors = {...foundErrors, lastLocation: 'Location coordinates is invalid'}
+      foundErrors = {...foundErrors, lastLocation: 'Location coordinates is invalid e.g. 24.212, -54.122'}
     }
 
     if (formData.description.length > 500) {
@@ -113,6 +102,7 @@ const NewReportPage = () => {
 
     if (Object.keys(foundErrors).length === 0) {
       // no errors!
+      console.log("making report")
       onCreateReportPress(formData)
     }
   }
@@ -120,6 +110,35 @@ const NewReportPage = () => {
   const closeAlert = () => {
     setIsCreated(false);
   };
+
+  const formatDatetimeString = (date) => {
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear().toString();
+
+    return `${hours}:${minutes} ${day}/${month}/${year}`;
+  };
+
+  const handleConfirm = (date) => {
+    hidePicker();
+    if (date) {
+      setSelectedDate(date);
+      setFormData({...formData, lastSeenDateTime: formatDatetimeString(date)})
+    }
+  };
+
+
+  var maximumDate;
+  const openPicker = () => {
+    maximumDate = new Date();
+    setShowPicker(true);
+  };
+
+  const hidePicker = () => {
+    setShowPicker(false);
+  }
 
   return (
       <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding">
@@ -155,12 +174,16 @@ const NewReportPage = () => {
                   {'missingPetId' in errors && <FormControl.ErrorMessage>{errors.missingPet}</FormControl.ErrorMessage>}
                 </FormControl>
 
-                <FormControl>
+                <FormControl isInvalid={'lastSeenDateTime' in errors}>
                   <FormControl.Label>Last Seen</FormControl.Label>
-                  <Button onPress={openPicker}>{selectedDatetime.toLocaleTimeString() + " " + selectedDatetime.toDateString()}</Button>
-                  <DateTimePickerModal date={selectedDatetime} isVisible={showPicker} mode="datetime" locale="en_GB" maximumDate={maximumDate} onConfirm={handleConfirm} onCancel={handleCancel} />
+                  <Button onPress={openPicker}>{`${selectedDatetime.getHours()}:${selectedDatetime.getMinutes()} ${selectedDatetime.toDateString()}`}</Button>
+                  <DateTimePickerModal date={selectedDatetime} isVisible={showPicker} mode="datetime" locale="en_GB" maximumDate={maximumDate} onConfirm={(date) => handleConfirm(date)} onCancel={hidePicker} />
+
+                  {/* <DatePicker date={selectedDatetime} mode="datetime" placeholder="Select date and time" maxDate={maximumDate} /> */}
                   {'lastSeenDateTime' in errors && <FormControl.ErrorMessage>{errors.lastSeenDateTime}</FormControl.ErrorMessage>}
                 </FormControl>
+
+
 
                 <FormControl isInvalid={'lastLocation' in errors}>
                   <FormControl.Label>Last Known Location</FormControl.Label>
