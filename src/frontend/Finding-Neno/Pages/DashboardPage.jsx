2dc7e3e8e903d1d32316ca506031ccd5cd740df7
sash-enka <sjus0001@student.monash.edu>
2023-08-11 17:35:12 +1000

    add sighting from dashboard WIP

--- src/frontend/Finding-Neno/Pages/DashboardPage.jsx
+++ src/frontend/Finding-Neno/Pages/DashboardPage.jsx
@@ -4,8 +4,12 @@ import {Dimensions} from 'react-native';
 import { Color } from "../components/atomic/Theme";
 import { useEffect, useState } from 'react';
 import { useIsFocused } from '@react-navigation/native';
+import * as ImagePicker from 'expo-image-picker';
+import { Picker } from '@react-native-picker/picker';
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
 
 import store from '../store/store';
+import { validDateTime, validateCoordinates } from "./validation"
 import { useSelector, useDispatch } from "react-redux";
 
 const DashboardPage = () => {
@@ -17,10 +21,36 @@ const DashboardPage = () => {
   const toast = useToast();
   const isFocused = useIsFocused();
 
+  // TODO: change report structure to be an array of dictionaries? Refer to mock data that is commented out for desired structure
+  const [reports, setReports] = useState([]);
   const [modalVisible, setModalVisible] = useState(false);
+  const [sightingDateTime, setSightingDateTime] = useState(new Date());
+  const [sightingData, setSightingData] = useState({authorId: USER_ID, lastLocation: '', description: ''});
+  const [reportSightingBtnDisabled, setReportSightingBtnDisabled] = useState(false);
+  const [sightingFormErrors, setSightingFormErrors] = useState({});
+  const [sightingImage, setSightingImage] = useState(null);
+  const [showPicker, setShowPicker] = useState(false);
 
-  const handlePress = () => {
+  const formatDatetime = (datetime) => {
+		const hours = datetime.getHours().toString().padStart(2, '0');
+		const minutes = datetime.getMinutes().toString().padStart(2, '0');
+		const day = datetime.getDate().toString().padStart(2, '0');
+		const month = (datetime.getMonth() + 1).toString().padStart(2, '0');
+		const year = datetime.getFullYear().toString();
+
+		return `${hours}:${minutes} ${day}/${month}/${year}`
+	}
+
+  const handlePress = (report) => {
     setModalVisible(!modalVisible);
+    setSightingData({ ...sightingData, 
+      missing_report_id: report[0], 
+      animal: report[7], 
+      breed: report[8],
+      image_url: null, 
+      dateTime: formatDatetime(sightingDateTime),
+      dateTimeOfCreation: formatDatetime(new Date()),
+    })
   };
 
   const handleConfirm = () => {
@@ -30,15 +60,92 @@ const DashboardPage = () => {
       placement: "top"
     })
   };
-    // TODO: change report structure to be an array of dictionaries? Refer to mock data that is commented out for desired structure
-    const [reports, setReports] = useState([]);
+
+    console.log(reports);
 
     useEffect(() => {
       if (isFocused) {
         fetchAllReports();
       }
     }, [isFocused]);
+    
+    // TODO: replace this image with the actual image from DB ? 
+    const image = "https://wallpaperaccess.com/full/317501.jpg";
+    //
+    //   const mocks = [{ownerName: 'Sashenka', petName:'Piggy', species: 'Dog', breed: 'Shiba', isActive: true, lastLocation: 'Clayton, Victoria', lastDateTime: '12th May, 12:45pm', petImage: "https://qph.cf2.quoracdn.net/main-qimg-46470f9ae6267a83abd8cc753f9ee819-lq"},
+    //               ]
+
+    const petImage = "https://qph.cf2.quoracdn.net/main-qimg-46470f9ae6267a83abd8cc753f9ee819-lq"
+
+    // validation
+    const validateDetails = (formData) => {
+      // Validates details. If details are valid, send formData object to onCreateReportPress.
+      foundErrors = {};
   
+      if (!formData.lastLocation || formData.lastLocation == "") {
+        foundErrors = { ...foundErrors, lastLocation: 'Last known location is required e.g. 24.212, -54.122' }
+      } else if (!validateCoordinates(formData.lastLocation)) {
+        foundErrors = { ...foundErrors, lastLocation: 'Location coordinates is invalid e.g. 24.212, -54.122' }
+      }
+  
+      if (formData.description.length > 500) {
+        foundErrors = { ...foundErrors, description: 'Must not exceed 500 characters' }
+      }
+  
+      setSightingFormErrors(foundErrors);
+      return Object.keys(foundErrors).length === 0;
+    }
+
+    // photo upload
+    const handleChoosePhoto = async () => {
+      const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+      if (status === 'granted') {
+        let result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.Images,
+          allowsEditing: true,
+          aspect: [4, 3],
+          quality: 1,
+        });
+        if (!result.canceled) {
+          setSightingImage(result.assets[0].uri.toString());
+          setSightingData({ ...sightingData, image_url: result.assets[0].uri.toString() });
+        }
+      }
+  };
+
+  const handleTakePhoto = async () => {
+    const { status } = await ImagePicker.requestCameraPermissionsAsync();
+    if (status === 'granted') {
+      let result = await ImagePicker.launchCameraAsync({
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
+      if (!result.canceled) {
+        setSightingImage(result.assets[0].uri.toString());
+        setSightingData({ ...sightingData, image_url: result.assets[0].uri.toString() });
+      }
+    }
+  };
+
+  // date picker 
+  var maximumDate;
+	const openPicker = () => {
+		maximumDate = new Date();
+		setShowPicker(true);
+	};
+
+  const closePicker = () => {
+		setShowPicker(false);
+	}
+
+  const handleDatetimeConfirm = (datetime) => {
+		setSightingDateTime(datetime);
+		setSightingData({ ...sightingData, dateTime: formatDatetime(datetime) });
+		closePicker();
+	}
+
+    // API calls 
     const fetchAllReports = async () => {
       try {
         const response = await fetch(`${IP}:${PORT}/get_missing_reports`);
@@ -48,32 +155,74 @@ const DashboardPage = () => {
         console.error(error);
       }
     };
-   // TODO: replace mock data with real data
-  const image = "https://wallpaperaccess.com/full/317501.jpg";
-//
-//   const mocks = [{ownerName: 'Sashenka', petName:'Piggy', species: 'Dog', breed: 'Shiba', isActive: true, lastLocation: 'Clayton, Victoria', lastDateTime: '12th May, 12:45pm', petImage: "https://qph.cf2.quoracdn.net/main-qimg-46470f9ae6267a83abd8cc753f9ee819-lq"},
-//                   {ownerName: 'Sash', petName:'Bunny', species: 'Rabbit', breed: 'RabbitBreed', isActive: true, lastLocation: 'Melbourne, Victoria', lastDateTime: '15th May, 1:45pm', petImage: "https://cf.ltkcdn.net/small-mammals/small-mammal-names/images/orig/322037-1600x1066-white-rabbit.jpg"},
-//                   {ownerName: 'Ana', petName:'Noni', species: 'Cat', breed: 'House cat', isActive: true, lastLocation: 'Melbourne, Victoria', lastDateTime: '15th May, 1:45pm', petImage:"https://images.saymedia-content.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:eco%2Cw_1200/MTk2NzY3MjA5ODc0MjY5ODI2/top-10-cutest-cat-photos-of-all-time.jpg"},
-//                   {ownerName: 'Alina', petName:'Liza', species: 'Dog', breed: 'Yorkshire Terrier', isActive: true, lastLocation: 'Berwick, Victoria', lastDateTime: '11th May, 11:00pm', petImage: "https://www.shutterstock.com/image-photo/cute-dog-photography-yorkshire-terrier-260nw-1792147286.jpg"},
-//                   {ownerName: 'Jason', petName:'Yoyo', species: 'Bird', breed: 'Parrot', isActive: true, lastLocation: 'Glen Waverley, Victoria', lastDateTime: '11th May, 1:00pm', petImage: "https://www.thesprucepets.com/thmb/iMtikD4KQeIl73kPe134Hu2TOH4=/4933x0/filters:no_upscale():strip_icc()/blue-budgie-511936470-dff4c0952d4a45ec80f9ac7f406cc71f.jpg"}
-//               ]
-//   const description = "cute and fluffy"
 
-    const petImage = "https://qph.cf2.quoracdn.net/main-qimg-46470f9ae6267a83abd8cc753f9ee819-lq"
+    const handleAddSighting = async () => {
+      setReportSightingBtnDisabled(true);
+      let isValid = validateDetails(sightingData);
+
+      if (isValid) {
+        const url = `${IP}:${PORT}/insert_new_sighting`;
+
+        await fetch(url, {
+          method: "POST",
+          headers: {"Content-Type": "application/json"},
+          body: JSON.stringify(sightingData),
+        })
+        .then((res) => {
+          if (res.status == 201) {
+            toast.show({
+              description: "Your sighting has been added, and the owner has been alerted",
+              placement: "top"
+            })
+            setReportSightingBtnDisabled(false);
+            setModalVisible(false);
+          }
+        })
+        .catch((error) => alert(error));
+      }
+    }
 
     return (
         <ScrollView style={{backgroundColor: 'white'}}>
 
           {/* REPORT SIGHTING MODAL */}
           <Modal isOpen={modalVisible} onClose={setModalVisible} >
-        <Modal.Content maxH="212">
+        <Modal.Content >
           <Modal.CloseButton />
-          <Modal.Header>Confirm sighting</Modal.Header>
+          <Modal.Header>Sighting details</Modal.Header>
           <Modal.Body>
+          <FormControl.Label>Photo</FormControl.Label>
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                {sightingImage && <Image source={{ uri: sightingImage }} style={{ width: 100, height: 100 }} alt='pet sighting image'/>}
+            </View>
+
+            <Button variant="ghost" onPress={handleChoosePhoto}>
+                Choose From Library
+            </Button>
+            <Button variant="ghost" onPress={handleTakePhoto}>
+                Take Photo
+            </Button>
             <ScrollView>
-              <Text>
-                Please confirm that you have made a sighting of this pet before we alert the owner.
-              </Text>
+              {/* form details */}
+              <FormControl >
+                <FormControl.Label>Date and Time of Sighting</FormControl.Label>
+                    <Button onPress={openPicker}>{`${sightingDateTime.getHours().toString().padStart(2, '0')}:${sightingDateTime.getMinutes().toString().padStart(2, '0')} ${sightingDateTime.toDateString()}`}</Button>
+                        <DateTimePickerModal date={sightingDateTime} isVisible={showPicker} mode="datetime" locale="en_GB" maximumDate={new Date()} themeVariant="light" display="inline"
+                        onConfirm={(datetime) => handleDatetimeConfirm(datetime)} onCancel={closePicker} />
+              </FormControl>
+
+              <FormControl isInvalid={'lastLocation' in sightingFormErrors}>
+                  <FormControl.Label>Location of Sighting</FormControl.Label>
+                  <Input onChangeText={value => setSightingData({ ...sightingData, lastLocation: value })} placeholder="long (-180 to 180), lat (-90 to 90)" />
+                  {'lastLocation' in sightingFormErrors && <FormControl.ErrorMessage>{sightingFormErrors.lastLocation}</FormControl.ErrorMessage>}
+              </FormControl>
+
+              <FormControl isInvalid={'description' in sightingFormErrors}>
+                  <FormControl.Label>Description (Additional Info)</FormControl.Label>
+                  <Input onChangeText={value => setSightingData({ ...sightingData, description: value })} />
+                  {'description' in sightingFormErrors && <FormControl.ErrorMessage>{sightingFormErrors.description}</FormControl.ErrorMessage>}
+              </FormControl>
+
             </ScrollView>
           </Modal.Body>
           <Modal.Footer>
@@ -83,8 +232,11 @@ const DashboardPage = () => {
             }}>
                 Cancel
               </Button>
-              <Button bgColor={Color.NENO_BLUE} onPress={() => handleConfirm()}>
-                Confirm 
+              <Button bgColor={Color.NENO_BLUE} 
+                disabled={reportSightingBtnDisabled} opacity={!reportSightingBtnDisabled ? 1 : 0.6}
+                onPress={() => handleAddSighting()}
+              >
+                Report sighting 
               </Button>
             </Button.Group>
           </Modal.Footer>
@@ -92,7 +244,7 @@ const DashboardPage = () => {
       </Modal>
 
 
-            {/* {reports.map(({missing_report_id, owner_name, pet_name, pet_animal, pet_breed, location_latitude, location_longitude, date_time, description}) => ( */}
+          {/* REPORTS */}
               {reports && reports.map((report, index) => (
                <View key={index} alignContent="center" paddingBottom={30}>
                <Box bg="#F5F5F5" borderRadius={15} padding={5} >
@@ -109,12 +261,8 @@ const DashboardPage = () => {
                      <Heading size = "sm">
                        {report[10]}
                      </Heading>
-                     {/* <Text style={{ color: 'black' }} fontSize="xs">{isHidden ? userPhoneHidden : userPhone}</Text> */}
                      </VStack>
                      <Box width={70}></Box>
-                     {/* <Button onPress={toggleVisibility}>
-                     <Text>Show/Hide</Text>
-                     </Button> */}
                  </HStack>
            
                  <Box height={5}></Box>
@@ -146,7 +294,7 @@ const DashboardPage = () => {
                        Breed
                      </Heading>
                      <Text fontSize="sm">
-                     {report[7]}
+                     {report[8]}
                      </Text>
                    </VStack>
                  </HStack>
@@ -183,7 +331,7 @@ const DashboardPage = () => {
                      
                  </HStack>
                  <VStack>
-                 <Button mt="2" bgColor={Color.NENO_BLUE} onPress={() => handlePress()}>
+                 <Button mt="2" bgColor={Color.NENO_BLUE} onPress={() => handlePress(report)}>
                     Report a sighting
                   </Button>
                  </VStack>
