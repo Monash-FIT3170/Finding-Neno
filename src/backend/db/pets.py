b92d126186dda0f41189dfc20a21256057bab28d
Zahin Shahriar Bin Khair <zahin.shahriar.82@gmail.com>
2023-08-12 22:03:21 +1000

    Get Pets does not return False anymore when there is 0 matching pets

--- src/backend/db/pets.py
+++ src/backend/db/pets.py
@@ -39,7 +39,7 @@ def get_all_pets(
         print(f"Query executed successfully: {query}")
     except Exception as e:
         print(f"Error while executing query: {e}")
-        return False
+        return []
 
     cur.close()
 
