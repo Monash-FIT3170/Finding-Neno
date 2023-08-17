a331164dd21e4688c476c07ac295d95430ba2b4b
Vincent Wijaya <vwij0004@student.monash.edu>
2023-08-17 23:39:30 +1000

    Google Maps is displayed on map page.

--- src/frontend/Finding-Neno/Pages/MapPage.jsx
+++ src/frontend/Finding-Neno/Pages/MapPage.jsx
@@ -1,11 +1,13 @@
 import { NavigationContainer, useNavigation  } from '@react-navigation/native';
-import React from 'react';
-import MapView from 'react-native-maps';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from 'react';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import { Image, StyleSheet, View } from 'react-native';
 
 import { useSelector, useDispatch } from "react-redux";
 import { Text } from 'react-native';
 import store from "../store/store";
+
+import au from "../assets/au.json"
 
 export default function MapPage() {
 	const {IP, PORT} = useSelector((state) => state.api)
@@ -13,16 +15,31 @@ export default function MapPage() {
     const navigation = useNavigation();
     // const windowWidth = Dimensions.get('window').width; 
     // const windowHeight = Dimensions.get('window').height;
+	const geocoder = new google.maps.Geocoder()
 
-    return (
-		<View style={StyleSheet.container}>
-			<MapView style={StyleSheet.map} initialRegion={{
-    latitude: 37.78825,
-    longitude: -122.4324,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421,
-  }}/>
-			
+	const imageURL = '';
+	const [mapRegion, setMapRegion] = useState({
+		latitude: -37.8136,
+		longitude: 144.9631,
+		latitudeDelta: 0.0922,
+		longitudeDelta: 0.0421,
+	})
+
+	const handleRegionChange = (region) => {
+		console.log(region);
+	}
+
+	return (
+		<View style={styles.container}>
+			<MapView provider={PROVIDER_GOOGLE} style={styles.map} initialRegion={mapRegion}
+				mapType={Platform.OS == "android" ? "none" : "standard"} onRegionChange={(region) => handleRegionChange(region)} >
+
+				<Marker coordinate={mapRegion} title='Marker'>
+					{/* <Image source={{ uri: imageURL }} /> */}
+				</Marker>
+
+			</MapView>
+
 		</View>
 	);
 }
@@ -33,6 +50,7 @@ const styles = StyleSheet.create({
 	},
 	map: {
 		width: '100%',
-		height: '50%',
+		height: '100%',
+		alignSelf: 'stretch',
 	}
 });
