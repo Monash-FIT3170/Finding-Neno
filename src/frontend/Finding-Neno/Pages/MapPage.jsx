c7f31e838446402526b2cd3d899b0c7e382c36bb
Vincent Wijaya <vwij0004@student.monash.edu>
2023-08-27 18:53:15 +1000

    Fixed retrieve report for map and readded button - Fixed new report redirection after completion

--- src/frontend/Finding-Neno/Pages/MapPage.jsx
+++ src/frontend/Finding-Neno/Pages/MapPage.jsx
@@ -2,7 +2,7 @@ import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import React, { useEffect, useState } from 'react';
 import { useIsFocused } from '@react-navigation/native';
 import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
-import { Switch,Image, StyleSheet, View } from 'react-native';
+import { Switch, Image, StyleSheet, View } from 'react-native';
 import { TouchableOpacity } from 'react-native';
 import { Button, Text } from 'react-native';
 import store from "../store/store";
@@ -29,22 +29,15 @@ export default function MapPage() {
 	const [sightings, setSightings] = useState([]);
 
 	// Radio button to toggle between reports (true) and sightings (false)
-	const[isViewReports, setIsViewReports] = useState(true);
+	const [isViewReports, setIsViewReports] = useState(true);
 
 	// Reloads data when map page is opened
-	// useEffect(() => {
-	// 	if (isFocused) {
-	// 		fetchReports();
-	// 	}
-	// }, [isFocused]);
-
-
 	useEffect(() => {
-        if (isFocused) {
+		if (isFocused) {
 			fetchData(mapRegion);
-        }
-    }, [isFocused]);
-	
+		}
+	}, [isFocused]);
+
 
 	// Initial map view is Melbourne. Delta is the zoom level, indicating distance of edges from the centre.
 	const [mapRegion, setMapRegion] = useState({
@@ -64,25 +57,24 @@ export default function MapPage() {
 	}
 
 	const handleViewToggle = (value) => {
-		if (value) {
-			setIsViewReports(true);
-			fetchData(mapRegion, true);
-		}
-		else {
-			setIsViewReports(false);
-			fetchData(mapRegion, false);
-		}
+		setIsViewReports(value);
+
 		fetchData(mapRegion);
 	}
 
-	const fetchData = (region,viewReports) => {
+
+	const onPressSearch = () => {
+		fetchData(mapRegion);
+	}
+
+	const fetchData = (region) => {
 		const longitude = region["longitude"];
 		const longitude_delta = region["longitudeDelta"];
 		const latitude = region["latitude"];
 		const latitude_delta = region["latitudeDelta"];
 
 
-		if (viewReports) {
+		if (isViewReports) {
 			console.log("fetching reports")
 			fetchReports(longitude, longitude_delta, latitude, latitude_delta);
 		}
@@ -99,7 +91,7 @@ export default function MapPage() {
 			const data = await response.json();
 			// setReports(data[0]);
 			console.log("Fetched Reports:", data); // Log fetched data
-      		setReports(Array.isArray(data[0]) ? data[0] : []);//new
+			setReports(Array.isArray(data[0]) ? data[0] : []);//new
 		} catch (error) {
 			console.error(error);
 		}
@@ -107,89 +99,93 @@ export default function MapPage() {
 
 
 	const fetchSightings = async (longitude, longitude_delta, latitude, latitude_delta) => {
-		try{
+		try {
 			const response = await fetch(`${IP.toString()}:${PORT.toString()}/get_sightings_in_area?long=${longitude}&long_delta=${longitude_delta}&lat=${latitude}&lat_delta=${latitude_delta}`);
 			const data = await response.json();
 			// setSightings(data[0]);
 			console.log("Fetched Sightings:", data); // Log fetched data
-      	setSightings(Array.isArray(data[0]) ? data[0] : []);
+			setSightings(Array.isArray(data[0]) ? data[0] : []);
 		} catch (error) {
 			console.error(error);
 		}
 	}
-	
+
 	return (
 		<View style={styles.container}>
-			    <MapView
-        ref={(ref) => this.mapView = ref}
-        provider={PROVIDER_GOOGLE}
-        style={styles.map}
-        initialRegion={mapRegion}
-        showCompass={true}
-        showsIndoors={false}
-        rotateEnabled={false}
-        loadingEnabled={true}
-        mapType={"standard"}
-        onRegionChangeComplete={(newRegion) => handleRegionChange(newRegion)}
-    >
-        {/* Render reports markers */}
-        {isViewReports && Array.isArray(reports) 
-            ? reports.map((report, index) => (
-                <Marker
-                    key={`${report[0]}_${report[3]}_${report[4]}`}
-                    title={report[6]}
-                    coordinate={{ longitude: report[3], latitude: report[4] }}
-                    onPress={() =>
-                        this.mapView.animateToRegion({
-                            longitude: report[3],
-                            latitude: report[4],
-                            longitudeDelta: 0.0015,
-                        })
-                    }
-                ></Marker>
-            ))
-            : null}
+			<MapView
+				ref={(ref) => this.mapView = ref}
+				provider={PROVIDER_GOOGLE}
+				style={styles.map}
+				initialRegion={mapRegion}
+				showCompass={true}
+				showsIndoors={false}
+				rotateEnabled={false}
+				loadingEnabled={true}
+				mapType={"standard"}
+				onRegionChangeComplete={(newRegion) => handleRegionChange(newRegion)}
+			>
+				{/* Render reports markers */}
+				{isViewReports && Array.isArray(reports)
+					? reports.map((report, index) => (
+						<Marker
+							key={`${report[0]}_${report[3]}_${report[4]}`}
+							title={report[6]}
+							coordinate={{ longitude: report[3], latitude: report[4] }}
+							onPress={() =>
+								this.mapView.animateToRegion({
+									longitude: report[3],
+									latitude: report[4],
+									longitudeDelta: 0.0015,
+								})
+							}
+						></Marker>
+					))
+					: null}
 
-        {/* Render sightings markers */}
-        {!isViewReports && Array.isArray(sightings) 
-            ? sightings.map((sighting, index) => (
-                <Marker
-                    key={`${sighting[0]}_${sighting[2]}_${sighting[3]}`}
-                    title={sighting[10]}
-                    coordinate={{ longitude: sighting[2], latitude: sighting[3] }}
-                    onPress={() =>
-                        this.mapView.animateToRegion({
-                            longitude: sighting[2],
-                            latitude: sighting[3],
-                            longitudeDelta: 0.0015,
-                        })
-                    }
-                ></Marker>
-            ))
-            : null}
-    </MapView>
+				{/* Render sightings markers */}
+				{!isViewReports && Array.isArray(sightings)
+					? sightings.map((sighting, index) => (
+						<Marker
+							key={`${sighting[0]}_${sighting[2]}_${sighting[3]}`}
+							title={sighting[10]}
+							coordinate={{ longitude: sighting[2], latitude: sighting[3] }}
+							onPress={() =>
+								this.mapView.animateToRegion({
+									longitude: sighting[2],
+									latitude: sighting[3],
+									longitudeDelta: 0.0015,
+								})
+							}
+						></Marker>
+					))
+					: null}
+			</MapView>
 
 
 			{/* Switch and label */}
-            <View style={styles.switchContainer}>
-			<Text style={styles.switchLabel}>{isViewReports ? 'Reports' : 'Sightings'}</Text>
-			<Switch
-				trackColor={{ false: "#767577", true: "#81b0ff" }}
-				thumbColor={isViewReports ? "#f5dd4b" : "#f4f3f4"}
-				value={isViewReports}
-				onValueChange={value => handleViewToggle(value)}
-			/>
-            </View>
+			<View style={styles.switchContainer}>
+				<Text style={styles.switchLabel}>{isViewReports ? 'Reports' : 'Sightings'}</Text>
+				<Switch
+					trackColor={{ false: "#767577", true: "#81b0ff" }}
+					thumbColor={isViewReports ? "#f5dd4b" : "#f4f3f4"}
+					value={isViewReports}
+					onValueChange={value => handleViewToggle(value)}
+				/>
+			</View>
 
 			{/* <VStack style={{position:'absolute', bottom:0, right:0, alignItems:'center', margin: 10, padding: 10, borderRadius: }} backgroundColor="grey"> */}
-			<View style={{position: 'absolute', top: 30}} alignItems='center'>
+			<View style={{ position: 'absolute', top: 30 }} alignItems='center'>
 
-			{
-				isViewReports ? <Text style={styles.boldText}> {reports.length} reports in area</Text> : <Text style={styles.boldText}> {sightings.length} sightings in area</Text>
-			}
+				{
+					isViewReports ? <Text style={styles.boldText}> {reports.length} reports in area</Text> : <Text style={styles.boldText}> {sightings.length} sightings in area</Text>
+				}
+
+				<TouchableOpacity style={styles.button} onPress={onPressSearch}>
+					<Text style={styles.buttonText}>Search this area</Text>
+				</TouchableOpacity>
 			</View>
-			{/* </VStack> */}
-			
+
+
 		</View>
 	);
 }
@@ -208,34 +204,34 @@ const styles = StyleSheet.create({
 	},
 
 	boldText: {
-        fontWeight: 'bold',
+		fontWeight: 'bold',
 		fontSize: 20
-    },
+	},
 
 	button: {
-        borderRadius: 20,
-        backgroundColor: 'blue',
-        paddingVertical: 10,
-        paddingHorizontal: 20,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    buttonText: {
-        fontWeight: 'bold',
-        color: 'white'
-    },
+		borderRadius: 20,
+		backgroundColor: 'blue',
+		paddingVertical: 10,
+		paddingHorizontal: 20,
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	buttonText: {
+		fontWeight: 'bold',
+		color: 'white'
+	},
 	switchContainer: {
-        flexDirection: 'column',
-        alignItems: 'center',
-        position: 'absolute',
-        top: 600,  // you can adjust this value
-        left: 145  // and this one too, to place it properly
-    },
+		flexDirection: 'column',
+		alignItems: 'center',
+		position: 'absolute',
+		top: 600,  // you can adjust this value
+		left: 145  // and this one too, to place it properly
+	},
 	switchLabel: {
-        fontSize: 24, 
-        fontWeight: 'bold',
-        marginRight: 10
-    }
+		fontSize: 24,
+		fontWeight: 'bold',
+		marginRight: 10
+	}
 
-	
+
 });
