288b592c83756fdbe85a64814e27460f203898b3
Rutvik Dave <rdav0013@student.monash.edu>
2023-08-27 18:08:01 +1000

    got rid test button from map page

--- src/frontend/Finding-Neno/Pages/MapPage.jsx
+++ src/frontend/Finding-Neno/Pages/MapPage.jsx
@@ -12,36 +12,9 @@ export default function MapPage() {
     const windowWidth = Dimensions.get('window').width; 
     const windowHeight = Dimensions.get('window').height;
 
-    const toggleMissingStatus = async () => {
-        try {
-            petId = 5;
-            const response = await fetch(`${IP}:${PORT}/toggle_missing_status`, {
-                method: 'POST',
-                headers: {
-                    Authorization: `Bearer ${ACCESS_TOKEN}`,
-                    'User-ID': USER_ID,
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ pet_id: petId }),
-            });
-    
-            if (response.ok) {
-                console.log('Status toggled successfully');
-                // Perform any necessary updates on the frontend
-            } else {
-                console.log('Error while toggling status:', response.statusText);
-            }
-        } catch (error) {
-            console.error('An error occurred:', error);
-        }
-    };
-
     return (
     <View>
-    <Button onPress={toggleMissingStatus}>
-        TEST BUTTON
-
-    </Button>
+        <Text>Map Page </Text>
     </View>
     )
 }
