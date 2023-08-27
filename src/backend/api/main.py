65cfc5b964753407eed674e5f4f7e5d524fdbe12
Vincent Wijaya <vwij0004@student.monash.edu>
2023-08-27 19:17:10 +1000

    Added viewing own reports on report page

--- src/backend/api/main.py
+++ src/backend/api/main.py
@@ -124,7 +124,8 @@ def get_missing_reports():
     [
         missing_report_id, date_time (last seen), description (additional info), location_longitude, location_latitude,
         pet_id, pet_name, pet_animal, pet_breed, image_url,
-        owner_id, owner_name, owner_email, owner_phone_number
+        owner_id, owner_name, owner_email, owner_phone_number,
+        author_id
     ]
     """
     author_id = request.args.get("author_id")
