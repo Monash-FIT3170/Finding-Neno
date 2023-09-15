9803027546363cea4eea0c661007b7ca3572ba8e
Josiah Schuller <jsch0049@student.monash.edu>
2023-09-15 11:48:24 +1000

    Restructured app.config.js build vars

--- src/frontend/Finding-Neno/app.config.js
+++ src/frontend/Finding-Neno/app.config.js
@@ -1,49 +1,56 @@
-module.exports = {
-  "expo": {
-    "name": "Finding-Neno",
-    "slug": "finding-neno",
-    "version": "1.0.0",
-    "orientation": "portrait",
-    "icon": "./assets/icon.png",
-    "userInterfaceStyle": "light",
-    "splash": {
-      "image": "./assets/splash.png",
-      "resizeMode": "contain",
-      "backgroundColor": "#ffffff"
-    },
-    "assetBundlePatterns": [
-      "**/*"
-    ],
-    "ios": {
-      "supportsTablet": true,
-      "bundleIdentifier": "com.josiahschuller.findingneno"
-    },
-    "android": {
-      "adaptiveIcon": {
-        "foregroundImage": "./assets/adaptive-icon.png",
+module.exports = () => {
+  let config = {
+    "expo": {
+      "name": "Finding-Neno",
+      "slug": "finding-neno",
+      "version": "1.0.0",
+      "orientation": "portrait",
+      "icon": "./assets/icon.png",
+      "userInterfaceStyle": "light",
+      "splash": {
+        "image": "./assets/splash.png",
+        "resizeMode": "contain",
         "backgroundColor": "#ffffff"
       },
-      "config": {
-        "googleMaps": {
-          "apiKey": process.env.GOOGLE_MAPS_API_KEY
-        }
+      "assetBundlePatterns": [
+        "**/*"
+      ],
+      "ios": {
+        "supportsTablet": true,
+        "bundleIdentifier": "com.josiahschuller.findingneno"
       },
-      "package": "com.josiahschuller.findingneno"
-    },
-    "web": {
-      "favicon": "./assets/favicon.png"
-    },
-    "extra": {
-      "eas": {
-        "projectId": process.env.EAS_PROJECT_ID
+      "android": {
+        "adaptiveIcon": {
+          "foregroundImage": "./assets/adaptive-icon.png",
+          "backgroundColor": "#ffffff"
+        },
+        "config": {
+          "googleMaps": {
+            "apiKey": process.env.GOOGLE_MAPS_API_KEY
+          }
+        },
+        "package": "com.josiahschuller.findingneno"
+      },
+      "web": {
+        "favicon": "./assets/favicon.png"
+      },
+      "runtimeVersion": {
+        "policy": "sdkVersion"
       }
-    },
-    "owner": "josiahschuller",
-    "runtimeVersion": {
-      "policy": "sdkVersion"
-    },
-    "updates": {
+    } 
+  }
+
+  if (process.env.EXPO_ENVIRONMENT === "build") {
+    config.extra = {
+      "eas": {
+        "projectId": "6b02a04a-94c0-4b03-baee-2a3b9739dc63"
+      }
+    };
+    config.owner = "josiahschuller";
+    config.updates = {
       "url": "https://u.expo.dev/6b02a04a-94c0-4b03-baee-2a3b9739dc63"
-    }
-  } 
+    };
+  }
+
+  return config;
 }
